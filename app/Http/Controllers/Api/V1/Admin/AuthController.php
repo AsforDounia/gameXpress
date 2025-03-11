35a9db66a87740542211914fbbec7846f0aec8dd
dounia <asfordounia@gmail.com>
2025-03-11 11:39:14 +0000

    Day 2: Implementation of authentication system (register, login, logout) with Sanctum

--- app/Http/Controllers/Api/V1/Admin/AuthController.php
+++ app/Http/Controllers/Api/V1/Admin/AuthController.php
@@ -3,9 +3,57 @@
 namespace App\Http\Controllers\Api\V1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Spatie\Permission\Models\Role;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+    }
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        $token = $user->createToken($user->name);
+
+        return [
+            'status' => 'success',
+            'message' => 'User logged in successfully',
+            'data' => [
+                'user' => $user,
+                'token' => $token->plainTextToken
+            ]
+        ];
+    }
+
+    public function logout(Request $request)
+    {
+
+        $request->user()->tokens()->delete();
+        return [
+            'status' => 'success',
+            'message' => 'User logged out successfully'
+        ];
+    }
 }
