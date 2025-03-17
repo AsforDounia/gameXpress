b73aa3e654ed16720a6abd22f42bfc8ef4e62117
soaud arziki <arziki.souad@gmail.com>
2025-03-17 20:30:47 +0000

    l'ajout du produit au panier

--- app/Http/Controllers/Api/V2/CartController.php
+++ app/Http/Controllers/Api/V2/CartController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
+use App\Models\CartItem;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {
@@ -22,6 +26,61 @@ class CartController extends Controller
     {
         //
     }
+    public function AddToCartGuest(Request $request){
+        $request->validate([
+            'product_id' => 'required|exists:products,id',
+            'quantity' => 'required|integer|min:1'
+        ]);
+
+        $cartData = [
+            'product_id' => $request->product_id,
+            'quantity' => $request->quantity,
+        ];
+
+        $cartData['user_id'] = null;
+        $cartData['session_id'] = Session::getId();
+
+        CartItem::create($cartData);
+
+        return response()->json(['message' => 'Item added to cart successfully.']);
+
+        return [
+            'product_id' => $product->id, 
+            'productImage' => $productImage,
+            'quantity' => $quantity
+        ];
+    }
+
+        public function addToCart(Request $request)
+        {
+            $request->validate([
+                'product_id' => 'required|exists:products,id',
+                'quantity' => 'required|integer|min:1',
+            ]);
+
+            $cartData = [
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity,
+            ];
+
+            $cartData['user_id'] = Auth::id();
+            $cartData['session_id'] = null;
+
+            $cartItem = CartItem::create($cartData);
+
+            $product = Product::with('images')->find($request->product_id);
+
+            return ['message' => 'Item added to cart successfully.',
+                'cart_Item' => [
+                    'product_id' => $product->id,
+                    'name' => $product->name,
+                    'price' => $product->price,
+                    'quantity' => $cartItem->quantity,
+                    'image' => $product->productImages->where('is_primary',true)
+                ],
+            ];
+        }
+    
 
     /**
      * Display the specified resource.
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Api\V1\Admin\ProductController;
 use App\Http\Controllers\Api\V1\Admin\CategoryController;
 use App\Http\Controllers\Api\V1\Admin\UserController;
 use App\Http\Controllers\Api\V1\Admin\SubcategoryController;
+use App\Http\Controllers\Api\V2\CartController;
 
 Route::prefix('v1')->group(function () {
 
@@ -39,7 +40,11 @@ Route::prefix('v1')->group(function () {
     });
 });
 
-
+Route::prefix('v2')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/addToCart', [CartController::class, 'addToCart']);
+    });
+});
 // Route::post('test', [AuthController::class, 'register']);
 
 // Route::middleware('auth:sanctum')->group(function () {
