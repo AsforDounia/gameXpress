804f42ff5a1982489a58d1e3276ac7cb674d0968
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-03-18 09:41:00 +0000

    GAM-20 cart items merging

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    protected $fillable = ['product_id','user_id','session_id','quantity'];
+    use HasFactory;
+    protected $fillable = ['product_id', 'user_id', 'session_id', 'quantity'];
 }
