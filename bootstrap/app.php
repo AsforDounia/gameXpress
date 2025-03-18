20d2bede976a22c6c76ab1def6040c6d94ce1762
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-03-18 12:51:46 +0000

    GAM-23

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withSchedule(function (Schedule $schedule) {
-        $schedule->job(new ExpireCartItems)->everyFiveSeconds();
+        $schedule->job(new ExpireCartItems)->hourly();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
