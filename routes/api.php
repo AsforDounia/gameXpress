43a2385a233c054f4c0875035c6aadff10841098
ikrambenallali <ikrambenallali2003@gmail.com>
2025-03-20 14:06:25 +0000

    fixe un erreur dans la fonction success

--- routes/api.php
+++ routes/api.php
@@ -82,7 +82,9 @@ Route::prefix('v3')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('orders', OrderController::class);
         Route::patch('orders/cancel/{order}', [OrderController::class, 'cancel'])->name('order.cancel');
+
         Route::get('/success', [PaymentController::class, 'success']);
+        Route::post('/checkout', [PaymentController::class, 'createCheckoutSession']);
     });
 });
 
