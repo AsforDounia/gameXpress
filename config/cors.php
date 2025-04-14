1dee7a0d2989f247a02c2bdb9876860399857243
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-04-14 14:36:27 +0100

    frontend

--- config/cors.php
+++ config/cors.php
@@ -18,11 +18,13 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_origins' => [
         'http://localhost:3000',
-        'http://localhost:3001', // Add this line
+        'http://localhost:3001',
+        'http://localhost:5173',
+
     ],
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Critical for authentication cookies
+    'supports_credentials' => true,
 ];
